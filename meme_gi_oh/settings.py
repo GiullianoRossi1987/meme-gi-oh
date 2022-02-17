2f5666b9d70c91495106997f738bf23836aaf221
GiullianoRossi1987 <giulliano.scatalon.rossi@gmail.com>
2022-02-17 10:50:50 -0300

    Cleaning project

--- meme_gi_oh/settings.py
+++ meme_gi_oh/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'meme_gi_oh.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': "meme_gi_oh",
+        'HOST': '127.0.0.1',
+        'USER': 'disc_mgo',
+        'PASSWORD': 'disc-fds',
+
     }
 }
 
